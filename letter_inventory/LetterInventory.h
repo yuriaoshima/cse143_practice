919d4f8c25e57146d204a254c2e34def1cde3116
Yuri Aoshima <teiyuri.aoshima@gmail.com>
2019-04-10 10:53:02 -0700

    added more functions

--- letter_inventory/LetterInventory.h
+++ letter_inventory/LetterInventory.h
@@ -6,15 +6,19 @@
 #include <iostream>
 
 class LetterInventory {
+
 public:
   LetterInventory(std::string data); //constructor, fills inventory array
   std::string toString(); //returns string representation of array with brackets
   bool isEmpty(); //returns if the inventory is empty or not
   int size(); //returns the total chars in inventory
+  int get(char letter);
+  void set(char letter, int value);
+  LetterInventory* add(LetterInventory other);
+
 private:
   int inventory[26]; //to hold count of each letter
   int totalChars;
-  bool empty; //tracks of the inventory is empty or not
 };
 
 #endif
